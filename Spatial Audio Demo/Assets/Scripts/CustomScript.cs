a6266735b30ef02b5972fc8e6dae85f3a0ab5a6a
icorki00 <ian.corkill@dmu.ac.uk>
2024-01-17 17:13:46 +0000

    Scripting
    Added a customer script for audio event from Wwise to play when a button is pressed. Removed contents and added it to the worksheet so students can try it out.

--- Spatial Audio Demo/Assets/Scripts/CustomScript.cs
+++ Spatial Audio Demo/Assets/Scripts/CustomScript.cs
@@ -6,64 +6,52 @@ using UnityEngine.UIElements;
 public class CustomScript : MonoBehaviour
 {
 
-    // Here we have created a number of Events for Wwise integration. 
+    // Here we have created an event for Wwise integration. 
     // As these are defined as public, we can see the events in the inspector and assign
     // an event too it, such as a play event.
     public AK.Wwise.Event myAudioOne;
-    public AK.Wwise.Event myAudioTwo;
-
-    /*
-     * A float is a floating point number. It is a numerical parameter which can 
-     * make use of decimal points.
-     */
+            
+    // A float is a floating point number. It is a numerical parameter which can 
+    // make use of decimal points. It can range from 1.17549e-38 to 3.40282e+38
     public float fTimer;
+    public float fCountdown;
 
-    public int iCountdown;
+    // An int is an Integer number. It is a numerical operator which can is a whole
+    // number. It can range from -32767 to 32767
+    int iRandom;
 
-    public Collider[] colliders;
+    // It is possible to use game objects as a parameter in your code.
+    // In the example below, this has actually been set up as an array; a container
+    // in which we store a number of different game objects. As this is a public
+    // parameter, the objects can be set in the inspector in Unity.
+    public GameObject[] headSpace;
 
-    public GameObject bottomObject;
-
+    // The start method is called at the beginning.
+    // You give parameters a default value in this function.
     private void Start()
     {
 
-        fTimer = 0;
-
+        
+              
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        GenerateAudio();
+        
 
     }
+
 
 
     public void GenerateAudio()
     {
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-
-            myAudioOne.Post(gameObject);
-
-            Debug.Log("Space was pressed");
-
-        }
-
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
         
-        if(collision.collider.CompareTag("FZone"))
-        {
-
-            myAudioTwo.Post(bottomObject);
-
-        }
 
     }
+
+    
 
 }
--- Spatial Audio Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Spatial Audio Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_LASER_1 = 711420983U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
